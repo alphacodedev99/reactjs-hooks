a2fce3226a98be3c225d7851d93f70bcf70fb62f
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-01-28 16:37:02 +0100

    test/form/terminal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import UseStateHook from './components/UseStateHook';
 import UseEffectHook from './components/UseEffectHook';
 function App() {
 	return (
+		// test git
 		<div className='App'>
 			<h1>React Hooks</h1>
 			<UseStateHook />
